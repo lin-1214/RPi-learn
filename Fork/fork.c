f2453001d15eb74601b48b31cd9eb96644242a69
lin-1214 <b10901059@g.ntu.edu.tw>
2022-11-24 16:25:40 +0800

    fork practice

---
+++ Fork/fork.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	pid_t pid_1 = fork();
+
+	if ( pid_1 == 0){
+		execl("/bin/ls", "ls", "-l",  NULL);
+	}
+	int status;
+	waitpid(pid_1, &status);
+	printf("Status: %d\n", status);
+	printf("Child PID: %d, end process.\n",pid_1);
+	return 0;
+}
---
+++ Fork/fork.md
@@ -0,0 +1,10 @@
+## call once return twice
+twice: parent & child
+
+fork(): return 
+	1. PID of child, in parent
+	2. 0, in child
+
+##process
+exec(), wait()
+
